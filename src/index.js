a840863b330221ce9e8c9cac98d52981ba943fcd
Nelson Mbau <sirmbau@gmail.com>
2019-06-22 14:04:40 +0300

    Added nodemon for dev

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ const app = express()
 const { PORT } = process.env
 
 app.get('/', (req, res) => res.json({ message: "Hello from Doc-Point api" }))
+app.get('/doctors', (req, res) => res.json({ message: "Doctor's endpoint" }))
 
 app.listen(PORT, () => console.log(`API running on port ${PORT}`))
