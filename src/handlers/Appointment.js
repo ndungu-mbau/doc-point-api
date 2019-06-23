16079e049f2b941dc4b28e21b3e9b4d0a4aa0123
Nelson Mbau <sirmbau@gmail.com>
2019-06-23 16:50:50 +0300

    Refactor handler classes to ensure links work

--- src/handlers/Appointment.js
+++ src/handlers/Appointment.js
@@ -1,9 +1,18 @@
-const { Appointment, Patient } = require('../models')
+const { Appointment, Patient, Doctor } = require('../models')
 
 const createAppointment = async (req, res) => {
+  const { doctor: doctorId, patient: patientId } = req.body
+  const doctor = await Doctor.findById(doctorId).exec()
+  const patient = await Patient.findById(patientId).exec()
+
   const newAppointment = new Appointment(req.body)
 
+  doctor.appointments.push(newAppointment._id)
+  patient.appointments.push(newAppointment._id)
+
   await newAppointment.save()
+  await doctor.save()
+  await patient.save()
 
   res.status(200).json(newAppointment)
 }
--- src/handlers/Doctor.js
+++ src/handlers/Doctor.js
@@ -1,13 +1,14 @@
 const { Hospital, Doctor } = require('../models')
 
 const addDoctor = async (req, res) => {
-  const { hospitalId, doctor } = req.body
+  const { hospital: hospitalId } = req.body
   const hospital = await Hospital.findById(hospitalId).exec()
 
-  const newDoctor = new Doctor({...doctor, hospital: hospital._id})
-  await newDoctor.save()
+  const newDoctor = new Doctor(req.body)
 
   hospital.doctors.push(newDoctor)
+
+  await newDoctor.save()
   await hospital.save()
 
   res.status(200).json(newDoctor)
