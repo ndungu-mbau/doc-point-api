c2d86109b7fc6120401feb71d2b4fc1444e5d51a
Nelson Mbau <sirmbau@gmail.com>
2019-06-23 13:54:38 +0300

    Add cookie-parser, body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const { PORT, DB_URI, HOST = '127.0.0.1' } = process.env
+
+mongoose.Promise = global.Promise
+mongoose.connect(DB_URI,{
+  useNewUrlParser: true
+})
+.then(()=> console.log(`Connected successfully on ${DB_URI}`))
+.catch(err => console.log(`Failed! ${err}`))
 
 const app = express()
-const { PORT } = process.env
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use((req, _, next) => {
+  req.db = mongoose.connection
+  next()
+})
 
 app.get('/', (req, res) => res.json({ message: "Hello from Doc-Point api" }))
-app.get('/doctors', (req, res) => res.json({ message: "Doctor's endpoint" }))
+app.get('/doctors', async (req, res) => {
+  res.json({ message: "Doctor's endpoint" })
+})
 
-app.listen(PORT, () => console.log(`API running on port ${PORT}`))
+app.listen(PORT, HOST, () => console.log(`API running on port ${PORT}`))
